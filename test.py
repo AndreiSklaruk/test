a05b1ecf22339e859822fc5175d1124dfab11b27
AndreiSklaruk <andrei.sklaruk@gmail.com>
2022-02-07 14:07:36 +0200

    commit from mac

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #Crop image and save
 from PIL import Image
 img = Image.open('image for croped/test.jpg')
-img = img.resize((1280, 852), Image.ANTIALIAS)
+img = img.resize((1280, 842), Image.ANTIALIAS)
 img.save('final/croped.jpg')
 img.show()
 
